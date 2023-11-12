ec4897bf555f4dc074cc89266838d1c9a1a14f99
Joseph Tran <115780760+Josephtran102@users.noreply.github.com>
2023-11-12 12:35:01 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -47,7 +47,7 @@ module.exports = {
       allowUnlimitedContractSize: true,
     },
     ethereum: {
-      url: "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      url: "https://rinkeby.infura.io/v3/YourAPIkey",
       chainId: 4,
       accounts: [process.env.PRIVATE_KEY],
       allowUnlimitedContractSize: true,
